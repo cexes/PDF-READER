4d5b67eac57331e7f68b19a2f7c70ccd77e3bb8c
cesxes <mottacesar001@gmail.com>
2023-09-11 19:28:36 -0300

    ADD A NOTE TABLE

---
+++ src/database/Models/DocumentsTable.js
@@ -0,0 +1,21 @@
+const pool = require('../databaseConnection');
+
+async function SelecAll () { 
+    const select = await pool.query('SELECT * FROM documents');
+    console.log(select.rows);
+};
+
+async function InsertPdf ( user_id, file_path) {
+    const insertPDF =  await pool.query ('INSERT INTO documents (user_id, file_path ) VALUES ($1, $2 )', 
+    [ user_id, file_path ] );
+    console.log(insertPDF);
+};
+
+async function InserNote ( user_id, file_path, annotations, page ) {
+   const insertNote = await pool.query ('INSERT INTO documents (user_id, file_path , annotations ,page ) VALUES ($1, $2, $3, $4)',
+   [user_id, file_path , annotations, page]);
+   console.log(insertNote);
+}
+
+SelecAll();
+//InserNote (1,'../teste ','EESSA PAGINA ME TAL', 1 );
