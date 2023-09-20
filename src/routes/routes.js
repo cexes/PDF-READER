3c9e389d994593b6cea4304830bfbae9596c1a1c
cesxes <mottacesar001@gmail.com>
2023-09-20 03:14:36 -0300

    add logout route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,10 +6,11 @@ const LoginController = require('../controller/LoginController');
 //Get Routes
 
 routes.get('/',(req,res) => { console.log('home')});
-routes.get('/teste', (req,res) => {
-     console.log(req.session.email_user)
-} )
-
+routes.get('/teste', (req,res) => { console.log(req.session.ident)});
+routes.get('/logout',(req,res) => { 
+    req.session.destroy() 
+    res.json('Sessão destruida')
+});
 //Post Routes
 routes.post('/create_user', LoginController.CreateUser)
 routes.post('/login', Session,  LoginController.LoginUser)
